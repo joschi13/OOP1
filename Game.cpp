dbbb152e87378c5a5a947b75e3ce751b103147f6
MICHAEL ZWEIMÜLLER <michael.zweimueller@student.tugraz.at>
2020-03-24 17:30:34 +0100

    First Test

--- Game.cpp
+++ Game.cpp
@@ -5,5 +5,27 @@
 //
 //------------------------------------------------------------------------------
 //
+#include <iostream>
+#include "Interface.hpp"
+#include "Game.hpp"
+
 
 using Oop::Game;
+
+Game::Game(Oop::Interface &io) : io_(io){}
+
+
+bool loadConfig(std::string config_file)
+{
+  if(/*invalid config file*/true)
+  {
+    std::cout << Oop::Interface::ERROR_INVALID_CONFIG  << std::endl;
+    return false;
+  }
+  //TO-DO
+}
+
+void run()
+{
+  //TO-DO
+}
--- Game.hpp
+++ Game.hpp
@@ -6,15 +6,39 @@
 //------------------------------------------------------------------------------
 //
 
+#ifndef INCLUDE_GAME_H
+#define INCLUDE_GAME_H
+
+
 namespace Oop
 {
   class Game
   {
-    private:
-      /* data */
-    public:
-      Game(/* args */);
-      ~Game();
+  private:
+
+    Oop::Interface& io_;
+    
+  public:
+    //------------------------------------------------------------------------
+    // Constructor
+    //
+    Game(Oop::Interface &io);
+    
+    //------------------------------------------------------------------------
+    // Destructor
+    //
+    virtual ~Game() noexcept;
+
+    //------------------------------------------------------------------------
+    // Deleted copy constructor
+    //
+    Game(const Game& original) = delete;
+
+    bool loadConfig(std::string config_file);
+
+    void run();
   };
   
 }
+
+#endif
