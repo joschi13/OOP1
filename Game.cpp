2cd95f17aa5e7d3093f2ef94cf31384cb565f060
MICHAEL ZWEIMÜLLER <michael.zweimueller@student.tugraz.at>
2020-03-25 19:28:09 +0100

    loadConfig00

--- Game.cpp
+++ Game.cpp
@@ -5,11 +5,21 @@
 //
 //------------------------------------------------------------------------------
 //
-#include <iostream>
+
+//* from 2_05.cpp
+
+#include <iostream>//*
+#include <fstream>//*
 #include "Interface.hpp"
 #include "Game.hpp"
+#include "rapidjson/document.h"
 
 
+using std::ofstream;//*
+using std::ifstream;//*
+using std::string;//*
+
+using namespace rapidjson;
 using Oop::Game;
 
 Game::Game(Oop::Interface &io) : io_(io){}
@@ -17,6 +27,10 @@ Game::Game(Oop::Interface &io) : io_(io){}
 
 bool loadConfig(std::string config_file)
 {
+  string json;
+  ifstream infile(config_file);
+  Document document;
+  document.Parse(json);
   if(/*invalid config file*/true)
   {
     std::cout << Oop::Interface::ERROR_INVALID_CONFIG  << std::endl;
