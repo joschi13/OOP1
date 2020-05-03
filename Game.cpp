9b08b0ce9399fc103a991a87a356bfd3f920a8f9
MICHAEL ZWEIMÜLLER <michael.zweimueller@student.tugraz.at>
2020-05-03 23:37:54 +0200

    attack command

--- Game.cpp
+++ Game.cpp
@@ -4,7 +4,7 @@
 // Group: Group 9, study assistant David Kerschbaumer
 //
 // Authors: Michael Zweimüller  11916150
-//          Johannes Aigner     11907005  
+//          Johannes Aigner     11907005
 //------------------------------------------------------------------------------
 //
 
@@ -23,14 +23,14 @@
 #include <sstream>
 #include <algorithm>
 #include <iterator>
-//#include <boost/algorithm/string.hpp>
+#include <stdlib.h>
 
-using std::ofstream;//*
-using std::ifstream;//*
-using std::string;//*
+using std::ifstream; //*
+using std::ofstream; //*
+using std::string;   //*
 
-using Oop::Interface;
 using Oop::Game;
+using Oop::Interface;
 using namespace rapidjson;
 
 //------------------------------------------------------------------------------
@@ -41,11 +41,11 @@ Game::Game(Oop::Interface &io) : io_(io)
 //------------------------------------------------------------------------------
 Game::~Game() noexcept
 {
-  for(auto const card : pick_up_stack)
+  for (auto const card : pick_up_stack)
   {
     delete card;
   }
-  
+
   delete players[0];
   delete players[1];
 }
@@ -54,140 +54,146 @@ Game::~Game() noexcept
 bool Game::loadConfig(std::string config_file)
 {
   Document doc;
-  SizeType index;  
+  SizeType index;
 
   std::string name;
   int stack_size, mana_costs, damage_points, life_points;
   bool shield, mana_drain;
-  const char * spell_name;
+  const char *spell_name;
   SpellType type;
 
-  Card* cur_card;
+  Card *cur_card;
 
   //read file
-  std::FILE* fptr = std::fopen(config_file.c_str(), "r");
+  std::FILE *fptr = std::fopen(config_file.c_str(), "r");
 
-  if (!fptr) {
+  if (!fptr)
+  {
     io_.error(Oop::Interface::ERROR_INVALID_CONFIG);
     return false;
   }
 
   std::fseek(fptr, 0, SEEK_END);
-  size_t len = size_t (std::ftell(fptr));
+  size_t len = size_t(std::ftell(fptr));
   std::fseek(fptr, 0, SEEK_SET);
   std::string content(len + 1, '\0');
   std::fread(&content[0], 1, len, fptr);
   fclose(fptr);
-  
+
   doc.Parse(content.c_str());
-  
+
   //check json structure
-  if((doc.IsObject()) && (doc.MemberCount() == 2) && \
-     (doc.HasMember("Creatures")) && (doc.HasMember("Spells"))){
-    Value& creatures = doc["Creatures"];
-    Value& spells = doc["Spells"];
+  if ((doc.IsObject()) && (doc.MemberCount() == 2) &&
+      (doc.HasMember("Creatures")) && (doc.HasMember("Spells")))
+  {
+    Value &creatures = doc["Creatures"];
+    Value &spells = doc["Spells"];
 
-    if((creatures.IsArray()) && (spells.IsArray()) && \
-       (stack_size = creatures.Size() + spells.Size() >= 10))
+    if ((creatures.IsArray()) && (spells.IsArray()) &&
+        (stack_size = creatures.Size() + spells.Size() >= 10))
 
-    {//check creatures
-      for(index = 0; index < creatures.Size(); index++)
+    { //check creatures
+      for (index = 0; index < creatures.Size(); index++)
       {
-        Value& temp = creatures[index];
-        if(temp.HasMember("name") && temp.HasMember("mana_cost") && \
-           temp.HasMember("damage_points") && temp.HasMember("life_points") &&  \
-           temp.HasMember("shield") && temp.HasMember("mana_drain"))
+        Value &temp = creatures[index];
+        if (temp.HasMember("name") && temp.HasMember("mana_cost") &&
+            temp.HasMember("damage_points") && temp.HasMember("life_points") &&
+            temp.HasMember("shield") && temp.HasMember("mana_drain"))
+        {
+
+          if ((temp["mana_cost"].IsInt()) && (temp["damage_points"].IsInt()) &&
+              (temp["life_points"].IsInt()) && temp["shield"].IsBool() &&
+              temp["mana_drain"].IsBool() && temp["name"].IsString() &&
+              (temp.MemberCount() == 6))
           {
+            name = temp["name"].GetString();
+            mana_costs = temp["mana_cost"].GetInt();
+            damage_points = temp["damage_points"].GetInt();
+            life_points = temp["life_points"].GetInt();
+            shield = temp["shield"].GetBool();
+            mana_drain = temp["mana_drain"].GetBool();
 
-            if((temp["mana_cost"].IsInt()) && (temp["damage_points"].IsInt()) && \
-               (temp["life_points"].IsInt()) && temp["shield"].IsBool() && \
-                temp["mana_drain"].IsBool() && temp["name"].IsString() && \
-               (temp.MemberCount() == 6))
+            if (inBetween(mana_costs, 1, 15) && inBetween(damage_points, 0, 9) &&
+                inBetween(life_points, 1, 9))
             {
-              name = temp["name"].GetString();
-              mana_costs = temp["mana_cost"].GetInt();
-              damage_points = temp["damage_points"].GetInt();
-              life_points = temp["life_points"].GetInt();
-              shield = temp["shield"].GetBool();
-              mana_drain = temp["mana_drain"].GetBool();
-              
-              if(inBetween(mana_costs, 1, 15) && inBetween(damage_points, 0, 9) && \
-                 inBetween(life_points, 1, 9))
-              {
-                cur_card = new CreatureCard(name, mana_costs, damage_points, life_points, \
-                                                 shield, mana_drain, false);
+              cur_card = new CreatureCard(name, mana_costs, damage_points, life_points,
+                                          shield, mana_drain, false);
 
-                if(!checkOnCreatureEquality(cur_card))
-                {
-                  delete cur_card;
-                  break;
-                }
-                
-                pick_up_stack.push_back(cur_card);
-              }else
+              if (!checkOnCreatureEquality(cur_card))
               {
+                delete cur_card;
                 break;
               }
-              
-                            
-            }else
+
+              pick_up_stack.push_back(cur_card);
+            }
+            else
             {
               break;
             }
-            
-          }else
+          }
+          else
           {
             break;
           }
+        }
+        else
+        {
+          break;
+        }
       }
 
-      if(index == creatures.Size())
-      {//check spells
-        for(index = 0; index < spells.Size(); index++)
+      if (index == creatures.Size())
+      { //check spells
+        for (index = 0; index < spells.Size(); index++)
         {
-          Value& temp = spells[index];
-          if(temp.HasMember("name") && temp["name"].IsString() && \
-            (temp.MemberCount() == 1))
+          Value &temp = spells[index];
+          if (temp.HasMember("name") && temp["name"].IsString() &&
+              (temp.MemberCount() == 1))
           {
             name = temp["name"].GetString();
             spell_name = name.c_str();
 
-            if(std::strcmp(spell_name, Interface::STRING_HEALER.c_str()))
+            if (std::strcmp(spell_name, Interface::STRING_HEALER.c_str()))
             {
-              if(std::strcmp(spell_name, Interface::STRING_RELIEF.c_str()))
+              if (std::strcmp(spell_name, Interface::STRING_RELIEF.c_str()))
               {
-                if(std::strcmp(spell_name, Interface::STRING_REBIRTH.c_str()))
+                if (std::strcmp(spell_name, Interface::STRING_REBIRTH.c_str()))
                 {
-                  if(std::strcmp(spell_name, Interface::STRING_DRACULA.c_str()))
+                  if (std::strcmp(spell_name, Interface::STRING_DRACULA.c_str()))
                   {
                     break;
-                  }else
+                  }
+                  else
                   {
                     type = Oop::DRACULA;
                   }
-                }else
+                }
+                else
                 {
                   type = Oop::REBIRTH;
                 }
-              }else
+              }
+              else
               {
                 type = Oop::RELIEF;
               }
-            }else
+            }
+            else
             {
               type = Oop::HEALER;
             }
-            
+
             cur_card = new SpellCard(type);
             pick_up_stack.push_back(cur_card);
-            
-          }else
+          }
+          else
           {
             break;
           }
         }
 
-        if(index == spells.Size())
+        if (index == spells.Size())
         {
           return true;
         }
@@ -206,26 +212,26 @@ bool Game::inBetween(int x, int low, int high)
 }
 
 //------------------------------------------------------------------------------
-bool Game::checkOnCreatureEquality(Card* card)
+bool Game::checkOnCreatureEquality(Card *card)
 {
-  CreatureCard* cur_creature;
-  CreatureCard* new_creature = dynamic_cast <CreatureCard*>(card);
+  CreatureCard *cur_creature;
+  CreatureCard *new_creature = dynamic_cast<CreatureCard *>(card);
 
-  for(Card* cur : pick_up_stack)
+  for (Card *cur : pick_up_stack)
   {
-    if(cur->getType() == Card::CardType::CREATURE && \
-      !(std::strcmp(cur->getName().c_str(), new_creature->getName().c_str())))
+    if (cur->getType() == Card::CardType::CREATURE &&
+        !(std::strcmp(cur->getName().c_str(), new_creature->getName().c_str())))
     {
-      cur_creature = dynamic_cast <CreatureCard*>(cur);
+      cur_creature = dynamic_cast<CreatureCard *>(cur);
 
-      if(!((cur_creature->getManaCost() == new_creature->getManaCost()) && \
-           (cur_creature->getLifePoints() == new_creature->getLifePoints()) && \
-           (cur_creature-> getShield() == new_creature->getShield()) && \
-           (cur_creature->getManaDrain() == new_creature->getManaDrain()) && \
-           (cur_creature->getSpeedy() == new_creature->getSpeedy()) && \
-           (cur_creature->getDamagePoints() == new_creature->getDamagePoints())))
+      if (!((cur_creature->getManaCost() == new_creature->getManaCost()) &&
+            (cur_creature->getLifePoints() == new_creature->getLifePoints()) &&
+            (cur_creature->getShield() == new_creature->getShield()) &&
+            (cur_creature->getManaDrain() == new_creature->getManaDrain()) &&
+            (cur_creature->getSpeedy() == new_creature->getSpeedy()) &&
+            (cur_creature->getDamagePoints() == new_creature->getDamagePoints())))
       {
-        return false;                                                                                                                                                                                                         
+        return false;
       }
     }
   }
@@ -235,39 +241,38 @@ bool Game::checkOnCreatureEquality(Card* card)
 //------------------------------------------------------------------------------
 bool Game::setupPlayer()
 {
-	players[0] = new Player();
-	players[1] = new Player();
+  players[0] = new Player();
+  players[1] = new Player();
 
-	players[0]->copyPickUpStack(pick_up_stack);
-	players[1]->copyPickUpStack(pick_up_stack);
-	
-	players[0]->shufflePickUpStack();
-	players[1]->shufflePickUpStack();
+  players[0]->copyPickUpStack(pick_up_stack);
+  players[1]->copyPickUpStack(pick_up_stack);
+
+  players[0]->shufflePickUpStack();
+  players[1]->shufflePickUpStack();
 
   players[0]->setName(io_.readPlayerName(0));
   players[1]->setName(io_.readPlayerName(1));
-  
+
   players[0]->takeOffCards(3);
   players[1]->takeOffCards(3);
 
-  
-	
-	return true;
+  return true;
 }
 
 //------------------------------------------------------------------------------
 void Game::run()
-{ 
+{
   int round_counter = -1;
   cur_player = 1;
 
   setupPlayer();
 
-  while(true)
+  while (true)
   {
     cur_player = cur_player ^ 1;
 
-    if(cur_player == 0){
+    if (cur_player == 0)
+    {
       round_counter++;
       io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::INFO_ROUND + std::to_string(round_counter));
     }
@@ -277,16 +282,13 @@ void Game::run()
     io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::INFO_CURRENT_PLAYER + players[cur_player]->getName());
 
     players[cur_player]->takeOffCards(1);
-   
+
     io_.out(players[cur_player], players[cur_player ^ 1]);
 
-
-    
-    if(!playerCommandInput())
+    if (!playerCommandInput())
     {
       return; //TODO
     }
-    
   }
 }
 
@@ -294,23 +296,28 @@ void Game::run()
 bool Game::playerCommandInput()
 {
   std::string input;
-  
-  while(true)
+
+  while (true)
   {
     io_.printCommandPrompt(players[cur_player]->getName());
     input = io_.in();
-    
-    if(compareCommandInput(Oop::Interface::COMMAND_QUIT, input))
+    if (!input.size())
     {
-      io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::ENDLINE_PART_ONE + \
-      players[cur_player ^ 1]->getName() + Oop::Interface::ENDLINE_PART_TWO);
+      continue;
+    }
+    
+
+    if (compareCommandInput(Oop::Interface::COMMAND_QUIT, input))
+    {
+      io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::ENDLINE_PART_ONE +
+                                                    players[cur_player ^ 1]->getName() + Oop::Interface::ENDLINE_PART_TWO);
       return false;
     }
 
-    if(compareCommandInput(Oop::Interface::COMMAND_HELP, input))
+    if (compareCommandInput(Oop::Interface::COMMAND_HELP, input))
     {
       input = Oop::Interface::INFO_HELP_MSGS.at(0);
-      for(size_t index = 1; index < Oop::Interface::INFO_HELP_MSGS.size(); index++)
+      for (size_t index = 1; index < Oop::Interface::INFO_HELP_MSGS.size(); index++)
       {
         input = input + "\n" + Oop::Interface::INFO_HELP_MSGS.at(index);
       }
@@ -318,36 +325,58 @@ bool Game::playerCommandInput()
       continue;
     }
 
-    if(compareCommandInput(Oop::Interface::COMMAND_STATE, input))
+    if (compareCommandInput(Oop::Interface::COMMAND_STATE, input))
     {
       io_.out(players[cur_player], players[cur_player ^ 1]);
       continue;
     }
 
-    if(compareCommandInput(Oop::Interface::COMMAND_FINISH, input))
+    if (compareCommandInput(Oop::Interface::COMMAND_FINISH, input))
     {
       return true;
-    }   
-
-    //strncasecmp: compares two strings caseinsensitive for n characters
-    if(!strncasecmp(Oop::Interface::COMMAND_ATTACK.c_str(), input.c_str(), 6))
-    {
-      //io_.out(Oop::Interface::OutputType::INFO, "test");
-      //if()
-      std::vector<std::string> arguments = tokenizeStr(input);
-
-      
-      //-for testing
-      for (size_t i = 0; i < arguments.size(); i++)
-      {
-        std::cout << arguments[i] << std::endl;
-      }
     }
 
+    //attack command
+    std::vector<std::string> arguments = tokenizeStr(input);
 
+    if (!strcasecmp(Oop::Interface::COMMAND_ATTACK.c_str(), arguments[0].c_str()))
+    {
+      long x = 0;
+      long y = 0;
+      if (arguments.size() != 4)
+      {
+        io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::WARNING_WRONG_PARAM_COUNT);
+        continue;
+      }
+      if ((x = std::strtol(arguments[1].c_str(), nullptr, 10)) && Game::inBetween(x, 0, 7) &&
+          !strcasecmp("with", arguments[2].c_str()) &&
+          (y = std::strtol(arguments[3].c_str(), nullptr, 10)) && Game::inBetween(y, 1, 7))
+      {
+        if (players[cur_player ^ 1]->getGameField()[x] == nullptr || players[cur_player]->getGameField()[y] == nullptr || !players[cur_player]->getGameField()[y]->getAlreadyAttacked())
+        {
+          io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::WARNING_EXECUTION_NOT_POSSIBLE);
+          continue;
+        }
+        else if (true /*validate if shields are up*/)
+        {
+          io_.out(Oop::Interface::OutputType::INFO, "test");
+          //TODO attack command itself
+        }
+      }
+      else
+      {
+        io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::WARNING_WRONG_PARAMETER);
+        continue;
+      }
+    }
+    else
+    {
+      io_.out(Oop::Interface::OutputType::INFO, Oop::Interface::WARNING_UNKNOWN_COMMAND);
+    }
   }
 
-  return false;;
+  return false;
+  ;
 }
 /*
 const std::string Interface::COMMAND_HELP = "help";
@@ -363,7 +392,7 @@ const std::string Interface::COMMAND_QUIT = "quit";
 //------------------------------------------------------------------------------
 bool Game::compareCommandInput(std::string cmd, std::string input)
 {
-  if(cmd.size() == input.size())
+  if (cmd.size() == input.size())
   {
     return !strcasecmp(cmd.c_str(), input.c_str());
   }
@@ -381,9 +410,6 @@ std::vector<std::string> Game::tokenizeStr(std::string input)
 {
   std::istringstream iss(input);
   std::vector<std::string> tokens{std::istream_iterator<std::string>{iss}, std::istream_iterator<std::string>{}};
-  
-  return tokens;
 
-  
-  
+  return tokens;
 }
